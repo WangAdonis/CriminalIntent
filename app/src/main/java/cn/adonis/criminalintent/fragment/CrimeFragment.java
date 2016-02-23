c95f0ab5742631603223fa47e41faeb7cc0ad8a5
WangAdonis <993825594@qq.com>
2016-02-23 22:01:45 +0800

    添加DatePickerFragment

--- app/src/main/java/cn/adonis/criminalintent/fragment/CrimeFragment.java
+++ app/src/main/java/cn/adonis/criminalintent/fragment/CrimeFragment.java
@@ -1,9 +1,11 @@
 package cn.adonis.criminalintent.fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 import cn.adonis.criminalintent.Crime;
@@ -28,6 +31,8 @@ public class CrimeFragment extends Fragment {
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
     public static final String EXTRA_CRIME_ID="cn.adonis.criminalintent.crime_id";
+    private static final String DIALOG_DATE="date";
+    private static final int REQUEST_DATE=0;
 
     public CrimeFragment() {
         // Required empty public constructor
@@ -67,7 +72,16 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm=getActivity().getSupportFragmentManager();
+//                DatePickerFragment dialog=new DatePickerFragment();
+                DatePickerFragment dialog=DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this,REQUEST_DATE);
+                dialog.show(fm,DIALOG_DATE);
+            }
+        });
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -90,4 +104,14 @@ public class CrimeFragment extends Fragment {
         getActivity().setResult(Activity.RESULT_OK,null);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode!=Activity.RESULT_OK)
+            return;
+        if(requestCode==REQUEST_DATE){
+            Date date=(Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            mDateButton.setText(mCrime.getDate().toString());
+        }
+    }
 }
